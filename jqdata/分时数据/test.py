2a5e4c4a5cd7354d4b28c51aa9def1e6e7f8ecdf
zhangjunyi111 <18435205109@163.com>
2024-01-02 07:28:58 +0800

    暂无

--- jqdata/分时数据/test.py
+++ jqdata/分时数据/test.py
@@ -1,17 +1,24 @@
 from openpyxl import load_workbook
 import pandas as pd
 import os
+import requests
+import json
+# code = 'sh600006'
+# df = pd.read_excel(f'sh600031.xlsx')
+# print(df)
+#
+# if os.path.exists(f'{code}.xlsx'):
+#     df_old = pd.read_excel(f'{code}.xlsx')
+#     df = pd.concat([df, df_old])
+#     df.to_excel(f'{code}.xlsx')
+# else:
+#     # pd.to_excel(f'{code}.xlsx'
+#     #             )
+#     df.to_excel(f'{code}.xlsx')
 
-
-code = 'sh600006'
-df = pd.read_excel(f'sh600031.xlsx')
-print(df)
-
-if os.path.exists(f'{code}.xlsx'):
-    df_old = pd.read_excel(f'{code}.xlsx')
-    df = pd.concat([df, df_old])
-    df.to_excel(f'{code}.xlsx')
-else:
-    # pd.to_excel(f'{code}.xlsx'
-    #             )
-    df.to_excel(f'{code}.xlsx')
+res = requests.post('https://web.ifzq.gtimg.cn/appstock/app/minute/query?code'
+                    '=sh603232')
+print(type(res.json()))
+print(res.json())
+with open('res.json', 'w') as f:
+    json.dump(res.json(), f, ensure_ascii=False, indent=4)
--- jqdata/分时数据/timeshare.py
+++ jqdata/分时数据/timeshare.py
@@ -6,9 +6,10 @@ import requests
 import json
 from sqlalchemy import create_engine
 import pymysql
-
+import datetime
 
 pymysql.install_as_MySQLdb()
+today = datetime.date.today()
 
 
 def create_mysql_engine() -> any:
@@ -28,10 +29,11 @@ def get_codes() -> list:
     """
     :return:涨停板的股票列表
     """
-    new_codes = ['sz001368', 'sz002067']
+    new_codes = []
     db_conn = create_mysql_engine()
     # 读取服务器的字典表
-    ztb_stock = pd.read_sql('select * from ztb_stock', db_conn)
+    ztb_stock = pd.read_sql(f"select mc_code from ztb_stock where trade_date "
+                            f"='2023-12-28'", db_conn)
     # 读取字段表中的ts_code列
     codes = ztb_stock[
         'mc_code'
@@ -47,6 +49,7 @@ def get_codes() -> list:
         elif code.startswith('6'):
             code = code.replace('6', 'sh6', 1)
             new_codes.append(code)
+    print('new_codes',new_codes)
     return new_codes
 
 
@@ -65,28 +68,30 @@ def get_result(new_codes: list):
     # 创建空的Dataframe准备接收返回的数据
     myresult = pd.DataFrame()
     for code in new_codes:
-        datas = []
-        url = f'https://web.ifzq.gtimg.cn/appstock/app/minute/query?code={code}'
-        res = requests.post(url=url, headers=headers, )
-        simWords = res.json()
+        try:
+            datas = []
+            url = f'https://web.ifzq.gtimg.cn/appstock/app/minute/query?code={code}'
+            res = requests.post(url=url, headers=headers, )
+            simWords = res.json()
+            # 根据simWords的格式进行解析
+            simWords = simWords['data'][code]['data']['data']
+            for data in simWords:
+                data = data.split(' ')
+                data.append(code)
+                data.append(date)
+                datas.append(data)
+            df = pd.DataFrame(datas,
+                              columns=['time', 'price', 'amount', 'vol', 'mccode',
+                                       'date'])
+            print(df)
+        except Exception as e:
+            continue
 
-        # 根据simWords的格式进行解析
-        simWords = simWords['data'][code]['data']['data']
-        for data in simWords:
-            data = data.split(' ')
-            data.append(code)
-            data.append(date)
-            datas.append(data)
-        df = pd.DataFrame(datas,
-                          columns=['time', 'price', 'amount', 'vol', 'mccode',
-                                   'date'])
-        print(df)
         # 利用pandas的contact函数将旧的结果与新结果拼接起来。
         myresult = pd.concat([myresult, df])
     myresult.to_sql(name='fs_data', con=db_conn2,
-                         if_exists='append',
-                         index_label=False, index=False)
-    # myresult.to_excel('分时数据2023-12-30.xlsx')
+                          if_exists='append',
+                          index_label=False, index=False)
     return
 
 
